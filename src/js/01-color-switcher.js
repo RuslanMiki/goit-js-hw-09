69f08958bfe56215b613e725b553269f5a9be786
RuslanMiki <mikitchenko_ruslan@ukr.net>
2022-11-27 15:30:49 +0100

    Task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const refs = {
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.stopButton.disabled = true;
+let timerId = null;
+
+refs.startButton.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    refs.body.style.backgroundColor = randomColor;
+  }, 1000);
+  refs.startButton.disabled = true;
+  refs.stopButton.disabled = false;
+});
+
+refs.stopButton.addEventListener('click', () => {
+  clearInterval(timerId);
+  refs.startButton.disabled = false;
+  refs.stopButton.disabled = true;
+});
 

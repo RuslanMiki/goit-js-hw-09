535e2c54b95c378614b92dcc94fe3b6ad149e802
RuslanMiki <99136935+RuslanMiki@users.noreply.github.com>
2022-11-27 14:31:27 +0000

    Deploying to gh-pages from @ RuslanMiki/goit-js-hw-09@69f08958bfe56215b613e725b553269f5a9be786 🚀

---
+++ 01-color-switcher.a94c5755.js
@@ -0,0 +1,2 @@
+const t={startButton:document.querySelector("[data-start]"),stopButton:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.stopButton.disabled=!0;let o=null;t.startButton.addEventListener("click",(()=>{o=setInterval((()=>{const o=`#${Math.floor(16777215*Math.random()).toString(16)}`;t.body.style.backgroundColor=o}),1e3),t.startButton.disabled=!0,t.stopButton.disabled=!1})),t.stopButton.addEventListener("click",(()=>{clearInterval(o),t.startButton.disabled=!1,t.stopButton.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.a94c5755.js.map
